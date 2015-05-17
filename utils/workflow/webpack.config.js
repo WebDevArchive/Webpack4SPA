d619eaaaf80468975f6948b50005da54a3b9547e
stas-webwork <stas.webwork@gmail.com>
2015-05-17 18:53:52 +0400

    UglifyJsPlugin

--- utils/workflow/webpack.config.js
+++ utils/workflow/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
 		]
 	},
 	plugins: [
-//		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
 
 		new ExtractTextPlugin("[name]/[name].css", {}),
 
